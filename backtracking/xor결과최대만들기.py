aa4ba06459a775c0de95d97053877feecce1040f
nbbeom <5877177a@naver.com>
2023-10-01 21:51:43 +0900

    2023-10-01

--- backtracking/xor결과최대만들기.py
+++ backtracking/xor결과최대만들기.py
@@ -1,8 +1,29 @@
-n ,m = map(int,input().split())
+import functools
 
-arr = [list(map(int,input().split()))]
-visited = []
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+a = list(map(int, input().split()))
 
-def xor():
-    for _ in range(m):
-        arr[]
+ans = 0
+
+
+def find_max_xor(curr_idx, cnt, curr_val):
+    global ans
+
+    if cnt == m:
+        ans = max(ans, curr_val)
+        return
+
+    if curr_idx == n:
+        return
+
+    # curr_idx index에 있는 숫자를 선택하지 않은 경우
+    find_max_xor(curr_idx + 1, cnt, curr_val)
+
+    # curr_idx index에 있는 숫자를 선택한 경우
+    find_max_xor(curr_idx + 1, cnt + 1, curr_val ^ a[curr_idx])
+
+
+find_max_xor(0, 0, 0)
+
+print(ans)
